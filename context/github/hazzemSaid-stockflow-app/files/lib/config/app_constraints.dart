758c76c2e9370286e132fec78df68e86db7decf5
Hazem Said <haazemsaidd@gmail.com>
2026-08-19 06:02:55 +0300

    chore: clean up .gitignore, untrack non-essential folders

--- context/github/hazzemSaid-stockflow-app/files/lib/config/app_constraints.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-class StockFlowConstraints {
-  static const double minDesignWidthDp = 360;
-  static const EdgeInsets screenPadding = EdgeInsets.all(16);
-}
-
